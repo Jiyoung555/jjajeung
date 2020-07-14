1b2e65ad2f0c7b33bcb60ae64226b4c8de295320
Jiyoung <wnsekd2333@gmail.com>
2020-07-14 18:07:58 +0900

    Initial commit

---
+++ src/test/java/com/example/jjajeung/JjajeungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jjajeung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjajeungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
